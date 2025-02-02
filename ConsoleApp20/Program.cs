aeb9f4534fc0975d53e3d32ffde24f3100808efb
RinAMFT <mucurkaarina@gmail.com>
2025-02-02 23:33:46 +0200

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int d = 50;  
+            int n = 10;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a + i * d;
+            }
+            Console.WriteLine($"Сумма накоплений через {n} месяцев: {sum} рублей");
+            Console.ReadKey();
+        }
+    }
+}
