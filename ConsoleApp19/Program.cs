7666ef29c31a2e3db08d55d41f2af0ca7c5f5bac
RinAMFT <mucurkaarina@gmail.com>
2025-02-02 23:24:36 +0200

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int d = 4;
+            int count = 1;
+            int sum = 0;
+            while (count <= 5)
+            {
+                Console.WriteLine(a);
+                sum += a;
+                a += d;
+                count++;
+            }
+            Console.WriteLine($"sum={sum}");
+            Console.Read();
+        }
+    }
+}
