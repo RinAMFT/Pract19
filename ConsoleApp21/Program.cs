9edb5ee3b82f72762433df569ea2a7a715a8d782
RinAMFT <mucurkaarina@gmail.com>
2025-02-02 23:43:11 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -200;
+            double d = 0.2;
+            int n = 150;
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a + i * d;
+            }
+            Console.WriteLine($"Сумма первых {n} членов арифметической прогрессии: {sum}");
+            Console.Read();
+        }
+    }
+}
