7af9f714b9b82500d38a278404044d41539b7263
RinAMFT <mucurkaarina@gmail.com>
2025-02-02 23:23:51 +0200

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int d = 4;
+            int count = 1;
+            while(count <=5)
+            {
+                Console.WriteLine(a);
+                a += d;
+                count++;
+            }
+            Console.Read();
+        }
+    }
+}
